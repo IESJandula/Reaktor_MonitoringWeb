50734e7bf765a63b3f634c9b54c735fe537b7cb2
PabloE-03 <PabloRuiz1312>
2024-03-21 18:39:53 +0100

    Se actualizan las peticiones

--- timetable/src/api/peticiones.js
+++ timetable/src/api/peticiones.js
@@ -64,6 +64,27 @@ export const getCourses = async ()=>{
     }
 }
 /**
+ * Metodo que realiza una peticion http al servidor para recoger 
+ * informacion sobre los tramos horarios
+ * @returns datos de los tramos horarios en formato json
+ */
+export const getTramos = async ()=>{
+    try
+    {
+        let url = "http://localhost:8088/horarios/get/tramos";
+        const response = await fetch(url);
+        if(!response.ok)
+        {
+            throw new Error("No se han cargado previamente ningun dato de los tramos");
+        }
+        return await response.json();
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+/**
  * Metodo que realiza una peticion http al servidor TimeTable para 
  * localizar a un profesor en tiempo real
  * @param {string} nombre 
@@ -85,6 +106,37 @@ export const getTeacherClassroom = async (nombre,apellido)=>{
         if(!response.ok)
         {
             throw new Error("Error al introducir el profesor");
+        }
+
+        return await response.json();
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+export const getTeacherClassroomHora = async (nombre,apellido,tramo)=>{
+    try
+    {
+        const params = {
+            name:nombre,
+            lastname: apellido
+        };
+
+        let url = "http://localhost:8088/horarios/teacher/get/classroom/tramo?"+new URLSearchParams(params).toString();
+
+        const response = await fetch(url,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+              },
+            body: JSON.stringify(tramo)
+        });
+
+        if(!response.ok)
+        {
+            throw new Error("Tramos no cargados o mal introducidos");
         }
 
         return await response.json();
